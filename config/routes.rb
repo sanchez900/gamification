2cb937007d7f2b3a69da96b28d583138e580630c
sanchez900 <patrickjayson900@yahoo.com>
2014-12-13 19:53:07 +0800

    customized the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login', to: 'devise/sessions#new', as: :login
+    get 'logout', to: 'devise/sessions#destroy', as: :logout
+  end
+
+
   resources :statuses do
     resources :users
   end
+
+  get 'feed', to: 'statuses#index', as: :feed
 
   root to: "statuses#index"
 
