dc5beb8711aad04d72f4cbbaa89ba56239a7ac3e
sanchez900 <patrickjayson900@yahoo.com>
2014-12-14 23:01:53 +0800

    added the show and the link to what ever you want in the profile and it shows the posts but still has a bug

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,9 +1,22 @@
 require 'test_helper'
 
 class ProfilesControllerTest < ActionController::TestCase
+  
   test "should get show" do
-    get :show
+    get :show, id: users(:patar).profile_name
     assert_response :success
+    assert_template 'profiles/show'
+  end
+
+  test "should render a 404 on profile not found" do
+  	get :show, id: "doesnt exist"
+  	assert_response :not_found
+  end
+
+  test "that variable are assigned on successful profile viewing" do
+    get :show, id: users(:patar).profile_name
+    assert assigns(:user)
+    assert_not_empty assigns(:statuses)
   end
 
 end
